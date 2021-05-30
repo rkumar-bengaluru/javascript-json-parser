08e2459f156e9a3c99d7f2c67a11648eab9c0b80
rkumar-bengaluru <rupak.kumar.ambasta@gmail.com>
2021-05-30 12:25:45 +0530

    testing jsonobject

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
-import JsonFormat from "./json/JsonFormat"
+import JsonObject from "./json/JsonObject"
 import css from "./css/style.css";
 
 parse();
 
 function parse() {
     try {
-        let json = new JsonFormat();
+        let instance = new JsonObject(false,false,"{\"name\"=\"rupak\"}");
         //var res = json.parse('{"rupak":"abcd"}')
         //console.log(res);
-        //document.getElementById('root').innerHTML = res;
+        document.getElementById('inputtextarea').innerHTML = instance.input;
     } catch (e) {
         console.error(e);
-        document.getElementById('root').innerHTML = e.message;
+        document.getElementById('inputtextarea').innerHTML = e.message;
     }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    verbose: true,
     mode: 'development',
     entry: {
         app: {
@@ -90,25 +89,5 @@ module.exports = {
         colors: true
     },
     devtool: 'source-map',
-    module: {
-        loaders: [
-          {exclude: ['node_modules'], loader: 'babel', test: /\.jsx?$/},
-          {loader: 'style-loader!css-loader', test: /\.css$/},
-          {loader: 'url-loader', test: /\.gif$/},
-          {loader: 'file-loader', test: /\.(ttf|eot|svg)$/},
-        ],
-      },
-      resolve: {
-        alias: {
-          config$: './configs/app-config.js',
-          react: './vendor/react-master',
-        },
-        extensions: ['', 'js', 'jsx'],
-        modules: [
-          'node_modules',
-          'bower_components',
-          'shared',
-          '/shared/vendor/modules',
-        ],
-      },
+    
 };
