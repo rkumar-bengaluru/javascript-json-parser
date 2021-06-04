f3bbba35b2b16a39a271c4ad731de79e24a74d9e
rkumar-bengaluru <rupak.kumar.ambasta@gmail.com>
2021-06-04 16:23:22 +0530

    json parser source done

---
+++ src/parser/RJsonAbsParser.js
@@ -0,0 +1,215 @@
+import JsonString from "../json/JsonString";
+import JsonBoolean from "../json/JsonBoolean";
+import JsonNumber from "../json/JsonNumber";
+import JsonNull from "../json/JsonNull";
+import RJsonConstants from "./RJsonConstants";
+
+export default class RJsonAbsParser extends RJsonConstants {
+
+    constructor(rinput) {
+        this.input = rinput;
+        this.reader = new StringReader(this.input);
+        this.stream = new CharStream(this.reader);
+        this.lexer = new RJsonLexer(this.stream);
+        this.token = new RJsonToken();
+        this.token.next = this.jj_nt = this.lexer.getNextToken();
+        this.jj_gen = 0;
+        this.jj_la1 = new Array(13);
+        this.jj_la1_0 = [0xccf8480, 0x78000, 0x1ccf8000, 0x40, 0x1ccf8000, 0x40, 0xccf8480, 0xccf8000, 0x60000, 0x18000, 0xcc00000, 0x8800000, 0x4400000,];;
+        var i;
+        for (i = 0; i < 13; i++)
+            this.jj_la1[i] = -1;
+
+        this.jj_kind = -1;
+        this.nativeNumbers = false;
+    }
+
+    getNextToken() {
+        if ((this.token = this.jj_nt).next != null) this.jj_nt = this.jj_nt.next;
+        else this.jj_nt = this.jj_nt.next = this.lexer.getNextToken();
+        this.jj_gen++;
+        return this.token;
+    }
+
+    objectKey() {
+        let o, key;
+        switch (this.jj_nt.kind) {
+            case this.STRING_SINGLE_EMPTY:
+            case this.STRING_DOUBLE_EMPTY:
+            case this.STRING_SINGLE_NONEMPTY:
+            case this.STRING_DOUBLE_NONEMPTY:
+                key = this.string();
+                break;
+            case SYMBOL:
+                key = this.symbol();
+                break;
+            case NULL:
+                this.nullValue();
+                key = null;
+                break;
+            case this.NUMBER_INTEGER:
+            case this.NUMBER_DECIMAL:
+            case this.TRUE:
+            case this.FALSE:
+                switch (this.jj_nt.kind) {
+                    case this.TRUE:
+                    case this.FALSE:
+                        o = this.booleanValue();
+                        break;
+                    case this.NUMBER_INTEGER:
+                    case this.NUMBER_DECIMAL:
+                        o = this.number();
+                        break;
+                    default:
+                        this.jj_la1[1] = this.jj_gen;
+                        this.jj_consume_token(-1);
+                        throw new Error();
+                }
+                key = o;
+                break;
+            default:
+                this.jj_la1[2] = this.jj_gen;
+                this.jj_consume_token(-1);
+                throw new Error();
+        }
+        { if (true) { key.key = true; return key; } }
+    }
+
+    string() {
+        let s;
+        switch (this.jj_nt.kind) {
+            case this.STRING_DOUBLE_EMPTY:
+            case this.STRING_DOUBLE_NONEMPTY:
+                s = this.doubleQuoteString();
+                break;
+            case this.STRING_SINGLE_EMPTY:
+            case this.STRING_SINGLE_NONEMPTY:
+                s = this.singleQuoteString();
+                break;
+            default:
+                this.jj_la1[10] = this.jj_gen;
+                this.jj_consume_token(-1);
+                throw new Error();
+        }
+        { if (true) return s; }
+    }
+
+    doubleQuoteString() {
+        switch (this.jj_nt.kind) {
+            case this.STRING_DOUBLE_EMPTY:
+                this.jj_consume_token(this.STRING_DOUBLE_EMPTY);
+                { if (true) return new JsonString(false, false, ""); }
+                break;
+            case STRING_DOUBLE_NONEMPTY:
+                this.jj_consume_token(this.STRING_DOUBLE_NONEMPTY);
+                var image = token.image;
+                { if (true) return new JsonString(false, false, image.substring(1, image.length() - 1)); }
+                break;
+            default:
+                this.jj_la1[11] = this.sjj_gen;
+                this.jj_consume_token(-1);
+                throw new Error();
+        }
+    }
+
+    singleQuoteString() {
+        switch (this.jj_nt.kind) {
+            case this.STRING_SINGLE_EMPTY:
+                this.jj_consume_token(this.STRING_SINGLE_EMPTY);
+                { if (true) return new JsonString(false, false, ""); }
+                break;
+            case this.STRING_SINGLE_NONEMPTY:
+                this.jj_consume_token(this.STRING_SINGLE_NONEMPTY);
+                var image = token.image;
+                { if (true) return new JsonString(false, false, image.substring(1, image.length() - 1)); }
+                break;
+            default:
+                this.jj_la1[12] = this.jj_gen;
+                this.jj_consume_token(-1);
+                throw new Error();
+        }
+    }
+
+    symbol() {
+        this.jj_consume_token(this.SYMBOL);
+        { if (true) return new JsonString(false, false, token.image); }
+    }
+
+    nullValue() {
+        this.jj_consume_token(this.NULL);
+        { if (true) return new JsonNull(); }
+    }
+
+    booleanValue() {
+        var b;
+        switch (this.jj_nt.kind) {
+            case this.TRUE:
+                this.jj_consume_token(this.TRUE);
+                b = true;
+                break;
+            case this.FALSE:
+                this.jj_consume_token(this.FALSE);
+                b = false;
+                break;
+            default:
+                this.jj_la1[8] = this.jj_gen;
+                this.jj_consume_token(-1);
+                throw new Error();
+        }
+        { if (true) return new JsonBoolean(b); }
+    }
+
+    value() {
+        let x;
+        switch (this.jj_nt.kind) {
+            case this.STRING_SINGLE_EMPTY:
+            case this.STRING_DOUBLE_EMPTY:
+            case this.STRING_SINGLE_NONEMPTY:
+            case this.STRING_DOUBLE_NONEMPTY:
+                x = this.string();
+                break;
+            case this.NUMBER_INTEGER:
+            case this.NUMBER_DECIMAL:
+                x = this.number();
+                break;
+            case this.TRUE:
+            case this.FALSE:
+                x = this.booleanValue();
+                break;
+            case this.NULL:
+                x = this.nullValue();
+                break;
+            default:
+                this.jj_la1[7] = this.jj_gen;
+                this.jj_consume_token(-1);
+                throw new Error();
+        }
+        { if (true) return x; }
+    }
+
+    number() {
+        let t;
+        switch (this.jj_nt.kind) {
+            case this.NUMBER_DECIMAL:
+                t = this.jj_consume_token(this.NUMBER_DECIMAL);
+                if (nativeNumbers) {
+                    { if (true) return new JsonNumber(false, false, t.image); }
+                } else {
+                    { if (true) return new JsonNumber(false, false, t.image); }
+                }
+                break;
+            case this.NUMBER_INTEGER:
+                t = this.jj_consume_token(NUMBER_INTEGER);
+                if (nativeNumbers) {
+                    { if (true) return new JsonNumber(false, false, t.image); }
+                } else {
+                    { if (true) return new JsonNumber(false, false, t.image); }
+                }
+                break;
+            default:
+                this.jj_la1[9] = this.jj_gen;
+                this.jj_consume_token(-1);
+                throw new Error();
+        }
+    }
+}
--- src/parser/RJsonParser.js
+++ src/parser/RJsonParser.js
@@ -1,28 +1,12 @@
-import RJsonLexer from "./RJsonLexer";
-import CharStream from "./CharStream";
-import StringReader from "./StringReader";
-import RJsonToken from "./RJsonToken";
-import RJsonConstants from "./RJsonConstants";
+import JsonMap from "../json/JsonMap";
+import JsonList from "../json/JsonList";
 
 var logger = require('../logger/logger');
 
-export default class RJsonParser extends RJsonConstants {
+export default class RJsonParser extends RJsonAbsParser {
 
     constructor(rinput) {
-        this.input = rinput;
-        this.reader = new StringReader(this.input);
-        this.stream = new CharStream(this.reader);
-        this.lexer = new RJsonLexer(this.stream);
-        this.token = new RJsonToken();
-        this.token.next = this.jj_nt = this.lexer.getNextToken();
-        this.jj_gen = 0;
-        this.jj_la1 = new Array(13);
-        this.jj_la1_0 = [0xccf8480,0x78000,0x1ccf8000,0x40,0x1ccf8000,0x40,0xccf8480,0xccf8000,0x60000,0x18000,0xcc00000,0x8800000,0x4400000,];;
-        var i;
-        for (i = 0; i < 13; i++) 
-            this.jj_la1[i] = -1;
-        
-        this.jj_kind = -1;
+        super(rinput);
     }
 
     parse() {
@@ -35,9 +19,131 @@ export default class RJsonParser extends RJsonConstants {
         return toReturn;
     }
 
+    anything() {
+        logger.debug("RJsonParser::anything()" + this.jj_nt.toString());
+        let x;
+        switch (this.jj_nt.kind) {
+            case this.BRACE_OPEN:
+                x = this.object();
+                break;
+            case this.BRACKET_OPEN:
+                x = this.list();
+                break;
+            case this.NUMBER_INTEGER:
+            case this.NUMBER_DECIMAL:
+            case this.TRUE:
+            case this.FALSE:
+            case this.NULL:
+            case this.STRING_SINGLE_EMPTY:
+            case this.STRING_DOUBLE_EMPTY:
+            case this.STRING_SINGLE_NONEMPTY:
+            case this.STRING_DOUBLE_NONEMPTY:
+                x = value();
+                break;
+            default:
+                this.jj_la1[0] = this.jj_gen;
+                this.jj_consume_token(-1);
+                throw new RJsonParserError();
+        }
+        { if (true) return x; }
+    }
+
+    object() {
+        let map = new JsonMap(true, false, new Map());
+        let key, value;
+        this.jj_consume_token(this.BRACE_OPEN);
+        switch (this.jj_nt.kind) {
+            case this.NUMBER_INTEGER:
+            case this.NUMBER_DECIMAL:
+            case this.TRUE:
+            case this.FALSE:
+            case this.NULL:
+            case this.STRING_SINGLE_EMPTY:
+            case this.STRING_DOUBLE_EMPTY:
+            case this.STRING_SINGLE_NONEMPTY:
+            case this.STRING_DOUBLE_NONEMPTY:
+            case this.SYMBOL:
+                key = this.objectKey();
+                this.jj_consume_token(COLON);
+                value = this.anything();
+                value.key = true;
+                map.put(key, value);
+                key = null; value = null;
+                label_1:
+                while (true) {
+                    switch (this.jj_nt.kind) {
+                        case this.COMMA:
+                            ;
+                            break;
+                        default:
+                            this.jj_la1[3] = this.jj_gen;
+                            break label_1;
+                    }
+                    this.jj_consume_token(this.COMMA);
+                    key = this.objectKey();
+                    this.jj_consume_token(this.COLON);
+                    value = this.anything();
+                    value.key = true;
+                    map.put(key, value);
+                    key = null; value = null;
+                }
+                break;
+            default:
+                this.jj_la1[4] = jj_gen;
+        }
+        this.jj_consume_token(this.BRACE_CLOSE);
+        { if (true) return map; }
+    }
+
+    list() {
+        let list = new JsonList(true, false, []);
+        logger.debug("RJsonParser::list()" + this.jj_nt.toString());
+        let value;
+        this.jj_consume_token(this.BRACKET_OPEN);
+        switch (this.jj_nt.kind) {
+            case this.BRACE_OPEN:
+            case this.BRACKET_OPEN:
+            case this.NUMBER_INTEGER:
+            case this.NUMBER_DECIMAL:
+            case this.TRUE:
+            case this.FALSE:
+            case this.NULL:
+            case this.STRING_SINGLE_EMPTY:
+            case this.STRING_DOUBLE_EMPTY:
+            case this.STRING_SINGLE_NONEMPTY:
+            case this.STRING_DOUBLE_NONEMPTY:
+                value = anything();
+                list.add(value);
+                value = null;
+                label_2:
+                while (true) {
+                    switch (this.jj_nt.kind) {
+                        case COMMA:
+                            ;
+                            break;
+                        default:
+                            this.jj_la1[5] = this.jj_gen;
+                            break label_2;
+                    }
+                    this.jj_consume_token(this.COMMA);
+                    value = this.anything();
+                    list.add(value);
+                    value = null;
+                }
+                break;
+            default:
+                this.jj_la1[6] = this.jj_gen;
+                ;
+        }
+        this.jj_consume_token(this.BRACKET_CLOSE);
+        {if (true) return list;}
+    }
+
+    
+
     ensureEOF() {
         this.jj_consume_token(0);
-        {if (true) return true;}
+        { if (true) return true; }
     }
 
     jj_consume_token(kind) {
@@ -62,7 +168,7 @@ export default class RJsonParser extends RJsonConstants {
         var jj_expentries = [];
         var la1tokens = [];
         var jj_expentry = [];
-        var i,j,k,l;
+        var i, j, k, l;
         if (jj_kind >= 0) {
             la1tokens[jj_kind] = true;
             jj_kind = -1;
@@ -70,7 +176,7 @@ export default class RJsonParser extends RJsonConstants {
         for (i = 0; i < 13; i++) {
             if (jj_la1[i] == jj_gen) {
                 for (j = 0; j < 32; j++) {
-                    if ((jj_la1_0[i] & (1<<j)) != 0) {
+                    if ((jj_la1_0[i] & (1 << j)) != 0) {
                         la1tokens[j] = true;
                     }
                 }
